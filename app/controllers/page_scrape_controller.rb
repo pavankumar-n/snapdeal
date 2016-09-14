4f1c269c8642f87b90f304ec8a3d2d45751531ef
pavan <pavan@pavan-VirtualBox.(none)>
2016-09-15 00:16:58 +0530

    update to add links time to crawl_time

--- app/controllers/page_scrape_controller.rb
+++ app/controllers/page_scrape_controller.rb
@@ -5,22 +5,28 @@ class PageScrapeController < ApplicationController
 	end
 
 	def scrape
-		url = params[:url]
-		@agent = Mechanize.new
-		start_time = Time.now
-    page = @agent.get(url)
-    @category = page.css('.category-name').text.gsub(/\s+/, " ")
-    @items = page.search('.js-tuple')
-    end_time = Time.now
-    crawl_time = (end_time - start_time).round(2)
-    # this is commented bcoz it creates new record even if same url is again submiited 
-    # metadatum = current_user.metadata.new
-    # metadatum.url = url
-    # metadatum.records_crawled = @items.count
-    # metadatum.crawl_time = Time.now
-    # metadatum.save!
+		if params[:url].present?
+			url = params[:url]
+			@agent = Mechanize.new
+			start_time = Time.now
+	    page = @agent.get(url)
+	    @category = page.css('.category-name').text.gsub(/\s+/, " ")
+	    @items = page.search('.js-tuple')
+	    end_time = Time.now
+	    crawl_time = (end_time - start_time)
+	    # this is commented bcoz it creates new record even if same url is again submiited 
+	    # metadatum = current_user.metadata.new
+	    # metadatum.url = url
+	    # metadatum.records_crawled = @items.count
+	    # metadatum.crawl_time = Time.now
+	    # metadatum.save!
 
-    #to avoid creating new record is same url is submitted.
-    current_user.metadata.find_or_initialize_by(:url => url).update_attributes!(:records_crawled => @items.count, :crawl_time => crawl_time)
+	    #to avoid creating new record is same url is submitted.
+	    current_user.metadata.find_or_initialize_by(:url => url).update_attributes!(:records_crawled => @items.count, :crawl_time => crawl_time)	
+		  @metadatum = current_user.metadata.find_by(:url => url)
+		else
+			flash[:notice] = "Please provide a URL"
+			redirect_to root_url
+		end
 	end
 end
--- app/models/metadatum.rb
+++ app/models/metadatum.rb
@@ -1,3 +1,8 @@
 class Metadatum < ActiveRecord::Base
   belongs_to :user
+
+  def update_crawl_time(total_time)
+  	self.crawl_time += total_time
+  	self.save!
+  end
 end
